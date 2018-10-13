92f7aeb6460dc983e905421d67ad0716f307ee5a
Andrei Kholkin <virtual.exe@mail.ru>
2018-10-14 01:43:41 +0700

    add solution for templator

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+namespace App;
+
+require 'vendor/autoload.php';
+
+$users = Generator::generate(100);
 
 $configuration = [
     'settings' => [
@@ -9,17 +13,38 @@ $configuration = [
 ];
 
 $app = new \Slim\App($configuration);
+
 $container = $app->getContainer();
 $container['renderer'] = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 
 $app->get('/', function ($request, $response) {
-    $response->write('Welcome to Slim!');
-    return $response;
+    return $this->renderer->render($response, 'index.phtml');
 });
 
-$app->get('/users/{id}', function ($request, $response, $args) {
-    $params = ['id' => $args['id']];
+// BEGIN (write your solution here)
+$app->get('/users', function ($request, $response) use ($users) {
+    $page = $request->getQueryParam('page', 1);
+    $per = $request->getQueryParam('per', 5);
+    $offset = ($page - 1) * $per;
+    $maxPages = (int) round(sizeof($users) / $per, 0, PHP_ROUND_HALF_UP);
+    $sliceUsers = array_slice($users, $offset, $per);
+
+    $params = [
+        'usersArray' => $sliceUsers,
+        'page' => $page,
+        'maxPages' => $maxPages
+        ];
+    return $this->renderer->render($response, 'users/index.phtml', $params);
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) use ($users) {
+    $id = (int) $args['id'];
+    $user = collect($users)->first(function ($value, $key) use ($id) {
+        return $value['id'] === $id;
+    });
+    $params = ['user' => $user];
     return $this->renderer->render($response, 'users/show.phtml', $params);
 });
 
 $app->run();
+// END
