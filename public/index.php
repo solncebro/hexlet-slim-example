e3b0e4d47a46e3e7df6859e20d8a369d31091ca4
Andrei Kholkin <virtual.exe@mail.ru>
2018-10-14 20:20:20 +0700

    add solution for forms

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+namespace App;
+
+require 'vendor/autoload.php';
+
+use function Stringy\create as s;
+
+$repo = new Repository();
 
 $configuration = [
     'settings' => [
@@ -9,17 +15,46 @@ $configuration = [
 ];
 
 $app = new \Slim\App($configuration);
+
 $container = $app->getContainer();
 $container['renderer'] = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 
 $app->get('/', function ($request, $response) {
-    $response->write('Welcome to Slim!');
-    return $response;
+    return $this->renderer->render($response, 'index.phtml');
 });
 
-$app->get('/users/{id}', function ($request, $response, $args) {
-    $params = ['id' => $args['id']];
-    return $this->renderer->render($response, 'users/show.phtml', $params);
+$app->get('/courses', function ($request, $response) use ($repo) {
+    $params = [
+        'courses' => $repo->all()
+    ];
+    return $this->renderer->render($response, 'courses/index.phtml', $params);
 });
+
+// BEGIN (write your solution here)
+$app->get('/courses/new', function ($request, $response) use ($repo) {
+    $params = [
+        'courses' => $repo->all()
+    ];
+    return $this->renderer->render($response, 'courses/new.phtml', $params);
+});
+
+$app->post('/courses', function ($request, $response) use ($repo) {
+    $validator = new Validator();
+    $course = $request->getParsedBodyParam('course');
+    $errors = $validator->validate($course);
+
+    if (empty($errors)) {
+        $repo->save($course);
+        return $response->withRedirect('/');
+    }
+
+    $params = [
+        'course' => $course,
+        'errors' => $errors
+    ];
+
+    return $this->renderer->render($response, 'courses/new.phtml', $params);
+});
+// END
 
 $app->run();
