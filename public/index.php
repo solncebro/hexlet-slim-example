3b6a427d3aacfb4f145bd8239fb5a88881cbcb09
Andrei Kholkin <virtual.exe@mail.ru>
2018-10-15 01:29:15 +0700

    add solution flash

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+session_start();
+
 $configuration = [
     'settings' => [
         'displayErrorDetails' => true,
@@ -9,17 +11,25 @@ $configuration = [
 ];
 
 $app = new \Slim\App($configuration);
+
 $container = $app->getContainer();
 $container['renderer'] = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+$container['flash'] = function () {
+    return new \Slim\Flash\Messages();
+};
 
+// BEGIN (write your solution here)
 $app->get('/', function ($request, $response) {
-    $response->write('Welcome to Slim!');
-    return $response;
+    $messages = $this->flash->getMessages();
+    $params = ['flash' => $messages];
+    return $this->renderer->render($response, 'index.phtml', $params);
 });
 
-$app->get('/users/{id}', function ($request, $response, $args) {
-    $params = ['id' => $args['id']];
-    return $this->renderer->render($response, 'users/show.phtml', $params);
+$app->post('/courses', function ($request, $response) {
+    $this->flash->addMessage('Success', 'Course Added');
+    
+    return $response->withStatus(302)->withHeader('Location', '/');
 });
+// END
 
 $app->run();
